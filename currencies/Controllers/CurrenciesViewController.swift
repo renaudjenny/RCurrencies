9eacb36d15a08f608b7f69cb94cb41df14e43967
Renaud Jenny <renaud.jenny@gmail.com>
2019-01-08 00:34:58 +0100

    feat: add first currency amount editing

--- currencies/Controllers/CurrenciesViewController.swift
+++ currencies/Controllers/CurrenciesViewController.swift
@@ -17,6 +17,11 @@ class CurrenciesViewController: UITableViewController {
     self.setupTimer()
   }
 
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    self.timer?.cancel()
+  }
+
   func setupTimer() {
     self.timer?.cancel()
     self.timer = DispatchSource.makeTimerSource()
@@ -26,22 +31,43 @@ class CurrenciesViewController: UITableViewController {
       guard let strongSelf = self else { return }
       strongSelf.ratesRepository.rates(baseCurrency: strongSelf.selectedCurrency) { rates in
         guard let rates = rates else { return }
-        strongSelf.currencies.update(rates: rates)
-        strongSelf.tableView.reloadData()
+        strongSelf.updateTable(with: rates)
       }
     }
     self.timer?.resume()
   }
 
-  override func viewWillDisappear(_ animated: Bool) {
-    super.viewWillDisappear(animated)
-    self.timer?.cancel()
+  func updateTable(with rates: Rates) {
+    let currenciesCountBeforeUpdate = self.currencies.count
+    self.currencies.update(rates: rates)
+    let isCurrenciesSizeChanged = currenciesCountBeforeUpdate != self.currencies.count
+    if isCurrenciesSizeChanged {
+      self.tableView.reloadData()
+    } else {
+      guard let visibleIndexPaths = self.tableView.indexPathsForVisibleRows else { return }
+      self.tableView.reloadRows(at: Array(visibleIndexPaths.dropFirst()), with: .none)
+    }
+  }
+
+  var firstCell: CurrencyTableViewCell? {
+    let firstIndexPath = IndexPath(row: 0, section: 0)
+    return self.tableView.cellForRow(at: firstIndexPath) as? CurrencyTableViewCell
+  }
+
+  func editFirstCellAmount() {
+    self.firstCell?.amountTextField.isUserInteractionEnabled = true
+    self.firstCell?.amountTextField.becomeFirstResponder()
+  }
+
+  func resignFirstCellResponder() {
+    self.firstCell?.amountTextField.resignFirstResponder()
   }
 }
 
 // MARK: Table View
 extension CurrenciesViewController {
   static let cellIdentifier = "CurrenciesViewControllerCellIdentifier"
+  static let rowAnimationDuration: TimeInterval = 0.4
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return self.currencies.count
@@ -61,18 +87,27 @@ extension CurrenciesViewController {
 
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     let firstIndexPath = IndexPath(row: 0, section: 0)
-    guard indexPath != firstIndexPath else { return }
+    guard indexPath != firstIndexPath else {
+      self.editFirstCellAmount()
+      return
+    }
 
     self.timer?.cancel()
+
+    self.firstCell?.amountTextField.isUserInteractionEnabled = false
 
     let currency = self.currencies[indexPath.row]
     self.baseAmount = self.baseAmount * currency.rate
     self.currencies.remove(at: indexPath.row)
     self.currencies.insert(currency, at: 0)
 
-    self.tableView.moveRow(at: indexPath, to: firstIndexPath)
-    self.tableView.scrollToRow(at: firstIndexPath, at: .top, animated: true)
-    self.setupTimer()
+    UIView.animate(withDuration: CurrenciesViewController.rowAnimationDuration, animations: {
+      self.tableView.moveRow(at: indexPath, to: firstIndexPath)
+      self.tableView.scrollToRow(at: firstIndexPath, at: .top, animated: true)
+    }) { _ in
+      self.editFirstCellAmount()
+      self.setupTimer()
+    }
   }
 }
 
@@ -80,6 +115,7 @@ extension CurrenciesViewController {
 extension CurrenciesViewController {
   override func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
     self.timer?.cancel()
+    self.resignFirstCellResponder()
   }
 
   override func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
