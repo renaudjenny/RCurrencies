7fe45ee64b97b24bed960f5a71a19ef6bdea9fa9
Renaud Jenny <renaud.jenny@gmail.com>
2019-01-09 19:24:57 +0100

    test: add first UI tests for first cell

--- currenciesUITests/currenciesUITests.swift
+++ currenciesUITests/currenciesUITests.swift
@@ -5,4 +5,25 @@ class currenciesUITests: XCTestCase {
     continueAfterFailure = false
     XCUIApplication().launch()
   }
+
+  func testEuroCell() {
+    let app = XCUIApplication()
+    let firstCell = app.tables.cells.firstMatch
+    XCTAssert(firstCell.exists)
+    let euroCellByCode = firstCell.staticTexts["EUR"]
+    XCTAssert(euroCellByCode.exists)
+    let euroCellByName = firstCell.staticTexts["Euro"]
+    XCTAssert(euroCellByName.exists)
+    let euroCellByFlag = firstCell.staticTexts["🇪🇺"]
+    XCTAssert(euroCellByFlag.exists)
+  }
+
+  func testEuroCellWithBaseAmount() {
+    let app = XCUIApplication()
+    let firstCell = app.tables.cells.firstMatch
+    XCTAssert(firstCell.exists)
+    let textField = firstCell.textFields.firstMatch
+    XCTAssert(textField.exists)
+    XCTAssertEqual(textField.value as? String, "100.00")
+  }
 }
