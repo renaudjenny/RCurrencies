65a298eb4875f796cc0807cf09b71b3769be89a3
Renaud Jenny <renaud.jenny@gmail.com>
2019-01-09 21:01:54 +0100

    test: add Update list UI tests

--- currenciesUITests/currenciesUITests.swift
+++ currenciesUITests/currenciesUITests.swift
@@ -1,6 +1,9 @@
 import XCTest
 
 class currenciesUITests: XCTestCase {
+  static let webserviceTimeout: TimeInterval = 5.0
+  static let serviceUpdateDelayWithDelta: TimeInterval = 1.1
+
   override func setUp() {
     continueAfterFailure = false
     XCUIApplication().launch()
@@ -25,5 +28,27 @@ class currenciesUITests: XCTestCase {
     let textField = firstCell.textFields.firstMatch
     XCTAssert(textField.exists)
     XCTAssertEqual(textField.value as? String, "100.00")
+  }
+
+  func testUpdateRows() {
+    let app = XCUIApplication()
+    let secondCell = app.tables.cells.element(boundBy: 1)
+    XCTAssert(secondCell.waitForExistence(timeout: currenciesUITests.webserviceTimeout))
+    let textField = secondCell.textFields.firstMatch
+    XCTAssert(textField.exists)
+    XCTAssertNotNil(textField.value as? String)
+    XCTAssertNotEqual(textField.value as? String, "0.00")
+  }
+
+  func testUpdateAutomaticallyRows() {
+    let app = XCUIApplication()
+    let secondCell = app.tables.cells.element(boundBy: 1)
+    XCTAssert(secondCell.waitForExistence(timeout: currenciesUITests.webserviceTimeout))
+    let textField = secondCell.textFields.firstMatch
+    let firstValue = textField.value as? String
+    XCTAssertNotNil(firstValue)
+    let textFieldChangeExpectation = expectation(for: NSPredicate(format: "value != \(firstValue!)"), evaluatedWith: textField)
+    wait(for: [textFieldChangeExpectation], timeout: currenciesUITests.serviceUpdateDelayWithDelta)
+    XCTAssertNotEqual(firstValue, textField.value as? String)
   }
 }
